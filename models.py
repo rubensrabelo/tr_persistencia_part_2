4643bcd1fe645a9b240b269a2d216fae88d9754c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-06 13:34:30 -0300

    Adding project model in the models file

---
+++ models.py
@@ -0,0 +1,36 @@
+from datetime import date
+from sqlmodel import SQLModel, Field
+
+
+class Project(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str
+    start_date: date = Field(default_factory=date.today)
+    end_date: date | None = Field(default=None)
+    completion_prediction: date
+    status: str
+
+
+# class Assignment(SQLModel, table=True):
+#     task_id: "Task"
+#     collaborator_id: "Collaborator"
+#     assignment_date: date
+
+
+# class Task(SQLModel, table=True):
+#     id: int | None
+#     name: str
+#     description: str
+#     start_date: date
+#     end_date: date | None
+#     completion_prediction: date
+#     status: str
+#     project_id: Project
+
+
+# class Collaborator(SQLModel, table=True):
+#     id: int | None
+#     name: int
+#     email: str
+#     function: str
