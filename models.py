b51edcb14c6d0a5e581e84e981276e89ab9709ca
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-10 16:02:49 -0300

    Adding task and assigment models

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
 
 class Project(SQLModel, table=True):
@@ -10,23 +10,27 @@ class Project(SQLModel, table=True):
     end_date: date | None = Field(default=None)
     completion_prediction: date
     status: str
+    tasks: list["Task"] = Relationship(back_populates="project")
 
 
-# class Assignment(SQLModel, table=True):
-#     task_id: "Task"
-#     collaborator_id: "Collaborator"
-#     assignment_date: date
+class Assignment(SQLModel, table=True):
+    task_id: int = Field(default=None, foreign_key="task.id", primary_key=True)
+    collaborator_id: int = Field(default=None, foreign_key="collaborator.id",
+                                 primary_key=True)
 
 
-# class Task(SQLModel, table=True):
-#     id: int | None
-#     name: str
-#     description: str
-#     start_date: date
-#     end_date: date | None
-#     completion_prediction: date
-#     status: str
-#     project_id: Project
+class Task(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str
+    start_date: date = Field(default_factory=date.today)
+    end_date: date | None = Field(default=None)
+    completion_prediction: date
+    status: str
+    project_id: int = Field(foreign_key="project.id")
+    project: Project = Relationship(back_populates="tasks")
+    collaborators = list["Collaborator"] = Relationship(back_populates="tasks",
+                                                        link_model=Assignment)
 
 
 class Collaborator(SQLModel, table=True):
@@ -34,3 +38,5 @@ class Collaborator(SQLModel, table=True):
     name: str
     email: str
     function: str
+    tasks = list[Task] = Relationship(back_populates="collaborators",
+                                      link_model=Assignment)
