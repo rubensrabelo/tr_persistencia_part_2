5f2b9bea82facb806295fab4a0ca531b3a31b2f9
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-19 20:08:15 -0300

    Leaving the project on an isolated router

--- api/routes/project.py
+++ api/routes/project.py
@@ -9,8 +9,6 @@ from datetime import datetime, timezone
 from database import get_session
 from models.project import Project
 from dto.project_dto import ProjecBaseWithTask
-from models.task import Task
-from dto.task_dto import TaskWithCollaborator
 
 router = APIRouter()
 
@@ -111,94 +109,3 @@ async def delete_project(project_id: int,
     session.delete(project)
     session.commit()
     return {"Message": "Project successfully deleted."}
-
-
-# Tasks
-@router.post("/{project_id}/tasks/",
-             response_model=Task)
-async def create_task_for_project(project_id: int,
-                                  task: Task,
-                                  session: Session = Depends(get_session)
-                                  ) -> Task:
-    project = session.get(Project, project_id)
-    if not project:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Project not found.")
-    task.project_id = project_id
-    session.add(task)
-    session.commit()
-    session.refresh(task)
-    return task
-
-
-@router.get("/{project_id}/tasks/",
-            response_model=list[TaskWithCollaborator])
-async def find_all_task_by_post_id(project_id: int,
-                                   offset: int = Query(default=0, ge=0),
-                                   limit: int = Query(default=10, le=100),
-                                   session: Session = Depends(get_session)
-                                   ) -> list[TaskWithCollaborator]:
-    project = session.get(Project, project_id)
-    if not project:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Project not found.")
-    statement = (select(Task)
-                 .where(Task.project_id == project_id)
-                 .offset(offset)
-                 .limit(limit)
-                 .options(joinedload(Task.collaborators)))
-    tasks_by_project = session.exec(statement).unique().all()
-    return tasks_by_project
-
-
-# Listar tasks por nome
-@router.get("/{project_id}/tasks/{name}",
-            response_model=list[TaskWithCollaborator])
-async def find_task_by_id(project_id: int,
-                          name: str,
-                          session: Session = Depends(get_session)
-                          ) -> list[TaskWithCollaborator]:
-    project = session.get(Project, project_id)
-    if not project:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Project not found.")
-    statement = (select(Task).where(Task.project_id == project_id,
-                                    Task.name.ilike(f"%{name}%"))
-                 .options(joinedload(Task.project),
-                          joinedload(Task.collaborators)))
-    task = session.exec(statement).unique().all()
-    if not task:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Task not found.")
-    return task
-
-
-@router.put("/{project_id}/tasks/{task_id}", response_model=Task)
-async def update_task(project_id: int,
-                      task_id: int,
-                      update_task: Task,
-                      session: Session = Depends(get_session)) -> Task:
-    task = session.get(Task, task_id)
-    if not task or task.project_id != project_id:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Task not found")
-    for key, value in update_task.model_dump(exclude_unset=True).items():
-        setattr(task, key, value)
-    task.updated_at = datetime.now(timezone.utc)
-    session.add(task)
-    session.commit()
-    session.refresh(task)
-    return task
-
-
-@router.delete("/{project_id}/tasks/{task_id}", response_model=dict)
-async def delete_task(project_id: int,
-                      task_id: int,
-                      session: Session = Depends(get_session)) -> dict:
-    task = session.get(Task, task_id)
-    if not task or task.project_id != project_id:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="Task not found")
-    session.delete(task)
-    session.commit()
-    return {"Message": "Task successfully deleted."}
