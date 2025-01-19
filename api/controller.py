5b0f0415cd90007fc0ea86182b85611a0ccd75cf
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-19 20:08:36 -0300

    Putting task router in the controller.py

--- api/controller.py
+++ api/controller.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 
 from .routes.project import router as project_router
+from .routes.task import router as task_router
 from .routes.collaborator import router as collaborator_router
 from .routes.statistic import router as statistic_router
 
 api_router = APIRouter()
 
 api_router.include_router(project_router, prefix="/projects", tags=["Posts"])
+api_router.include_router(task_router, prefix="/tasks", tags=["Tasks"])
 api_router.include_router(collaborator_router, prefix="/collaborators",
                           tags=["Collaborator"])
 api_router.include_router(statistic_router, prefix="/statistic",
