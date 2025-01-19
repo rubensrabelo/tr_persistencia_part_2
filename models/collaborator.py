2a9fff4ffc6f559fc115e55b5ca3ba04da18c305
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-19 08:32:04 -0300

    Removing CollaboratorWithTasks method in collaborator models

--- models/collaborator.py
+++ models/collaborator.py
@@ -17,7 +17,3 @@ class CollaboratorBase(SQLModel):
 class Collaborator(CollaboratorBase, table=True):
     tasks: list["Task"] = Relationship(back_populates="collaborators",
                                        link_model=Assignment)
-
-
-class CollaboratorWithTasks(CollaboratorBase):
-    tasks: list["Task"] = None
